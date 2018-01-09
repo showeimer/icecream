eb78f497bfffa57de0056d334f15dbaa4ae8e7dd
showeimer <showeimer@gmail.com>
2018-01-09 12:25:45 -0500

    deleted IceCream model, added PUT endpoint for restocking inventory

--- models/IceCream.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
-
-const iceCreamSchema = new Schema({
-  type: String,
-  stock: { type: Number, default: 100 }
-});
-
-module.exports = iceCreamSchema;
--- routes/truckRoutes.js
+++ routes/truckRoutes.js
@@ -3,6 +3,27 @@ const Truck = mongoose.model('trucks');
 
 module.exports = app => {
 
+  // CREATE ENDPOINTS ============================================
+
+  // Create a new ice cream truck
+  app.post('/api/trucks', async (req, res) => {
+    const { truckNumber, location } = req.body;
+    const truck = new Truck({
+      truckNumber,
+      location,
+    });
+
+    try {
+      await truck.save();
+      res.send(truck);
+    } catch (err) {
+      res.status(422).send(err);
+    }
+  });
+
+
+  // READ ENDPOINTS ================================================
+
   // Read all trucks in database
   app.get('/api/trucks', async (req, res) => {
     const trucks = await Truck.find({});
@@ -15,30 +36,24 @@ module.exports = app => {
     res.send(truck);
   });
 
-  // Create a new ice cream truck
-  app.post('/api/trucks', async (req, res) => {
-    const { truckNumber, location, flavors } = req.body;
-    const truck = new Truck({
-      truckNumber,
-      location,
-      flavors
-    });
 
-    try {
-      await truck.save();
-      res.send(truck);
-    } catch (err) {
-      res.status(422).send(err);
-    }
-  });
+  // UPDATE ENDPOINTS ===================================================================
 
   // Update inventory after a sale
   app.put('/api/trucks/:truckID/sale', async (req, res) => {
-
     // Parse through the sale, see what ice creams were sold, and how many
     for(let item in req.body) {
-      // Find the truck ID, and update data with second argument
+      // Find the truck ID, and decrement units sold
       await Truck.updateOne({ _id: req.params.truckID }, { $inc: { [item]: req.body[item] * -1 } })
+    }
+    const truck = await Truck.findById(req.params.truckID);
+    res.send(truck);
+  });
+
+  // Update inventory after resupply
+  app.put('/api/trucks/:truckID/restock', async (req, res) => {
+    for(let item in req.body) {
+      await Truck.updateOne({ _id: req.params.truckID }, { $inc: { [item]: req.body[item] } })
     }
     const truck = await Truck.findById(req.params.truckID);
     res.send(truck);
