21b57dd1e9a52da3ce47074cea42d2db47e04ae8
showeimer <showeimer@gmail.com>
2018-01-10 14:24:50 -0500

    installed bootstrap

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -21,7 +21,7 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
+      <div className="container">
         {this.renderGoogleMap()}
       <TruckDashboard trucks={this.props.trucks} />
       </div>
